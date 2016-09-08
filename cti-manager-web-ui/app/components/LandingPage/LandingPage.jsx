4c037b46940fa88094a43c535ad061f29725d2b5
whiteout <whiteoutdev@gmail.com>
2016-09-08 12:29:21 +0100

    Improves the landing page

--- cti-manager-web-ui/app/components/LandingPage/LandingPage.jsx
+++ cti-manager-web-ui/app/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+import appConfig from '../../config/app.config';
+
+import './LandingPage.scss';
+
 class LandingPage extends React.Component {
     render() {
         return (
             <div className="LandingPage">
-                <Link to="/media">Media</Link>
+                <h1 className="main-heading">{appConfig.appName}</h1>
+                <ul className="landing-page-list">
+                    <li className="landing-page-list-item">
+                        <Link to="/media">
+                            <i className="material-icons">image</i>
+                            <span>View Media</span>
+                        </Link>
+                    </li>
+                    <li className="landing-page-list-item">
+                        <Link to="/tags">
+                            <i className="material-icons">label</i>
+                            <span>Search Tags</span>
+                        </Link>
+                    </li>
+                </ul>
             </div>
         );
     }
