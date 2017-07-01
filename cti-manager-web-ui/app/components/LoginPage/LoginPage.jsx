e02f05a7b6b40432d567539e1f62048be62de7b2
whiteout <whiteoutdev@gmail.com>
2017-07-01 13:50:50 +0100

    Login page styling

--- cti-manager-web-ui/app/components/LoginPage/LoginPage.jsx
+++ cti-manager-web-ui/app/components/LoginPage/LoginPage.jsx
@@ -18,18 +18,27 @@ export default class LoginPage extends React.Component {
     render() {
         return (
             <div className="LoginPage">
-                <h1>{appConfig.appName}</h1>
-                <h2>Please log in</h2>
-                <form className="login-form" onSubmit={this.login.bind(this)}>
-                    <label htmlFor="username-input">Username:</label>
-                    <input id="username-input" name="username" type="text" ref={input => this.usernameInput = input}/>
-                    <label htmlFor="password-input">Password:</label>
-                    <input id="password-input"
-                           name="password"
-                           type="password"
-                           ref={input => this.passwordInput = input}/>
-                    <button action="submit">Login</button>
-                </form>
+                <div className="container">
+                    <h1>{appConfig.appName}</h1>
+                    <h2>Please log in</h2>
+                    <form className="login-form" onSubmit={this.login.bind(this)}>
+                        <div className="form-row">
+                            <label htmlFor="username-input">Username:</label>
+                            <input id="username-input"
+                                   name="username"
+                                   type="text"
+                                   ref={input => this.usernameInput = input}/>
+                        </div>
+                        <div className="form-row">
+                            <label htmlFor="password-input">Password:</label>
+                            <input id="password-input"
+                                   name="password"
+                                   type="password"
+                                   ref={input => this.passwordInput = input}/>
+                        </div>
+                        <button className="primary" action="submit">Login</button>
+                    </form>
+                </div>
             </div>
         );
     }
